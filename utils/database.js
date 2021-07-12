aa7ec8750e44dadf3e30e760f32f68b1aadbc88c
MamedYahyayev <yahyayev2001@gmail.com>
2021-07-12 19:19:45 +0400

    logging functionality established

--- utils/database.js
+++ utils/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("company_management", "mamed", "mamed2001", {
   dialect: "mysql",
   host: "localhost",
+  logging: console.log,
 });
 
 module.exports = sequelize;
